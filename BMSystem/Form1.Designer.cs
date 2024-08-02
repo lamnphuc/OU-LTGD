b7b3f2e3fe403f9e5806bd3390ac6009dd60de91
Hoàng Thế <151012645+ZtHoang@users.noreply.github.com>
2024-08-02 15:49:30 +0700

    Refactor Form1.Designer.cs to remove unused panel1 control

--- BMSystem/Form1.Designer.cs
+++ BMSystem/Form1.Designer.cs
@@ -28,22 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(431, 148);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -51,8 +42,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
     }
 }
 
