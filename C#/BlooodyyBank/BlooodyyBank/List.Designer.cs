ce22f91b147a6555ee7a1e17525716649e5adf97
Nguyen Lam <151693978+lamnphuc@users.noreply.github.com>
2024-09-07 16:48:36 +0700

    Add List form

--- C#/BlooodyyBank/BlooodyyBank/List.Designer.cs
+++ C#/BlooodyyBank/BlooodyyBank/List.Designer.cs
@@ -37,9 +37,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(5, 7);
+            this.dataGridView1.Location = new System.Drawing.Point(10, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(590, 368);
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.Size = new System.Drawing.Size(1180, 708);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
@@ -47,31 +49,34 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(38, 397);
+            this.label1.Location = new System.Drawing.Point(76, 763);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.Size = new System.Drawing.Size(347, 37);
             this.label1.TabIndex = 1;
             this.label1.Text = "Too much data to find?";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(259, 394);
+            this.button2.Location = new System.Drawing.Point(518, 758);
+            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 29);
+            this.button2.Size = new System.Drawing.Size(268, 56);
             this.button2.TabIndex = 3;
             this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // List
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "List";
-            this.Text = "Form5";
+            this.Text = "List of Donators";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
